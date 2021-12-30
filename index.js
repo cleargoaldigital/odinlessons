9f4c8fc79594648971e77a9f0eeb43219922abaf
Taofeek Ajibade <81028891+cleargoaldigital@users.noreply.github.com>
2021-12-30 06:23:41 +0100

    update index.js

--- index.js
+++ index.js
@@ -166,3 +166,19 @@ lastLetter("abcd") should return "d" */
   return result;
 }
 lastLetter("taofeek");
+
+// Fizzbuzz
+
+let userInput = parseInt(prompt("Please enter the number you'd like to FizzBuzz up to: "));
+
+for (let i = 1; i <= userInput; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
